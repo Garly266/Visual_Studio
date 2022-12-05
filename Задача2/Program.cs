a046ff05b6199aff8d98eeeb6622161938de163e
Garly <tolya-lisichkin@yandex.ru>
2022-12-06 00:56:44 +0400

    Задача 2

--- Задача2/Program.cs
+++ Задача2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int c = int.Parse(Console.ReadLine());
+
+
+int max = a;
+
+if ( b > max )
+{
+    Console.WriteLine($"Максимальное число {b}");
+}
+else if ( c > max )
+{
+    Console.WriteLine($"Максимальное число {c}"); 
+}
+else
+{
+    Console.WriteLine($"Максимальное число {max}");
+}
+
