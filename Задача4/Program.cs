d8bb62e573e566074e7303e859d87bd63cb3589c
Garly <tolya-lisichkin@yandex.ru>
2022-12-06 01:29:49 +0400

    Задача4

---
+++ Задача4/Program.cs
@@ -0,0 +1,11 @@
+﻿int N = int.Parse(Console.ReadLine());
+
+int i = 0;
+
+while (i <= N)
+{
+    Console.WriteLine($"Четное число {i%2==0}");
+    i=i+2;
+}
+
+Console.WriteLine("Нет четных чисел");
