9175e51c1db997d35a47986b75a1eb987c900e05
Garly <tolya-lisichkin@yandex.ru>
2022-12-06 01:14:20 +0400

    Задача3

---
+++ Задача3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine($"Число нечетное");
+}
+else 
+{
+    Console.WriteLine($"Число {a} является четным");
+} 
