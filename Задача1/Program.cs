368eae22703efe560f182a6bb938155a94e5f25b
Garly <tolya-lisichkin@yandex.ru>
2022-12-06 00:43:33 +0400

    Задача 1

---
+++ Задача1/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if (a < b)
+{
+    Console.WriteLine($"Максимальное значение {b}, минимальное {a}");
+}
+else if (a < b)
+{
+    Console.WriteLine($"Максимальное значение {a}, минимальное {b}");
+}
