1b24a0d92dab058f7864f0ad07f7b31f5b35ef2f
Garly <tolya-lisichkin@yandex.ru>
2022-12-11 16:46:10 +0400

    Задача 15

---
+++ Задача 15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int day = int.Parse(Console.ReadLine());
+
+if (day > 7 )
+{
+    Console.WriteLine("Неправильный ввод");
+    return;
+}
+if (day == 6 || day == 7 )
+{
+    Console.Write("Выходной");
+}
+else
+{
+    Console.Write("Будний день");
+}
