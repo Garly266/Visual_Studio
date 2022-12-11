9e69c357fcbe500eaa9f032479186d376c50f3c7
Garly <tolya-lisichkin@yandex.ru>
2022-12-12 02:30:31 +0400

    Задача 29

---
+++ Задача 29/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Заполните массив");
+int length = 8;
+int[] array = new int[length];
+
+    for (int i = 0; i < array.Length; ++i )
+    {
+      array [i] = int.Parse(Console.ReadLine());
+    }
+Console.WriteLine($"Массив:[{string.Join(", ", array)}].");
