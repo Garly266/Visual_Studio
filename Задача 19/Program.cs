62057dcdb953d27018af07753919ceb2c9c0372d
Garly <tolya-lisichkin@yandex.ru>
2022-12-11 19:38:41 +0400

    Задача 19

--- Задача 19/Program.cs
+++ Задача 19/Program.cs
@@ -7,21 +7,16 @@ if ( a.Length > 5 )
 }
 if(a.Length <= 5 )
 {
-    if (a[0] == a[4])
+    if (a[0] == a[4] && a[1] == a[3] )
     {
-        if (a[1] == a[3])
-        {
-            Console.WriteLine($"Число {a} является палиндромом");
-        }
+       Console.WriteLine($"Число {a} является палиндромом");
+        
     }
-    if (a[0] != a[4])
+    else
     {
         Console.WriteLine($"Число {a} не является палиндромом");
     }
-    if (a[1] != a[3])
-    {
-        Console.WriteLine($"Число {a} не является палиндромом");
-    }
+   
 }
 
 
