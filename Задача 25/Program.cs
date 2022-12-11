29d5ee96d4cb18dd8017d173a9ac6fdbaca5a495
Garly <tolya-lisichkin@yandex.ru>
2022-12-12 00:48:00 +0400

    Задача 25

---
+++ Задача 25/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = ReadInt("Введите число: ");
+int b = ReadInt("Введите степень: ");
+
+double result = Math.Pow(a, b);
+Console.WriteLine($"Число {a} в степени {b} = {result}");
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+
+    if (int.TryParse(Console.ReadLine(), out int i))
+    {
+        return i;
+    }
+    Console.WriteLine(" No message");
+    return -1;
+}
