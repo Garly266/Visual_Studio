ee0cf61d9398805e94d07d0016c0f5ef0fb03b4b
Garly <tolya-lisichkin@yandex.ru>
2022-12-11 20:22:59 +0400

    задача 23

---
+++ Задача 23/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = ReadInt("Введите число N: ");
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write($"{i * i * i} ");
+}
+
+
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+
+    if (int.TryParse(Console.ReadLine(), out int i))
+    {
+        return i;
+    }
+    Console.WriteLine(" No message");
+    return -1;
+}
