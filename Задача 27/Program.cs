eaf7d8dc7a819eb3b3484a935f5bd9cb8bd5b674
Garly <tolya-lisichkin@yandex.ru>
2022-12-12 01:06:00 +0400

    Задача 27

---
+++ Задача 27/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = ReadInt("Введите число");
+int Sum = 0;
+while (a > 0)
+{
+    Sum = Sum + a % 10;
+    a = a/ 10;
+}
+Console.WriteLine($"Сумма цифр в числе = {Sum}");
+
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+
+    if (int.TryParse(Console.ReadLine(), out int i))
+    {
+        return i;
+    }
+    Console.WriteLine(" No message");
+    return -1;
+}
