a2c2cf594b1cd1c3d302f4f18826867befb74564
Garly <tolya-lisichkin@yandex.ru>
2022-12-11 19:59:48 +0400

    задача 21

---
+++ Задача 21/Program.cs
@@ -0,0 +1,26 @@
+﻿int x1 = ReadInt("Введите координату X1: ");
+int y1 = ReadInt("Введите координату Y1: ");
+int z1 = ReadInt("Введите координату Z1: ");
+int x2 = ReadInt("Введите координату X2: ");
+int y2 = ReadInt("Введите координату Y2: ");
+int z2 = ReadInt("Введите координату Z2: ");
+
+int a = x2 - x1;
+int b = y2 - y1;
+int c = z1 - z2;
+
+double length = Math.Sqrt(a * a + b * b + c * c);
+Console.WriteLine($"Расстояние между точками : {length}");
+
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+
+    if ( int.TryParse(Console.ReadLine(), out int i))
+    { 
+        return i; 
+    }
+    Console.WriteLine(" No message");
+    return -1;
+}
