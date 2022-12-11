eb256f7ae0328274c03cf75d7f21561494255209
Garly <tolya-lisichkin@yandex.ru>
2022-12-11 19:30:43 +0400

    задача 19

---
+++ Задача 19/Program.cs
@@ -0,0 +1,34 @@
+﻿System.Console.WriteLine("Введите пятизначное число");
+string a = Console.ReadLine();
+int length = a.Length;
+if ( a.Length > 5 )
+{
+    Console.WriteLine("Неправильный ввод");
+}
+if(a.Length <= 5 )
+{
+    if (a[0] == a[4])
+    {
+        if (a[1] == a[3])
+        {
+            Console.WriteLine($"Число {a} является палиндромом");
+        }
+    }
+    if (a[0] != a[4])
+    {
+        Console.WriteLine($"Число {a} не является палиндромом");
+    }
+    if (a[1] != a[3])
+    {
+        Console.WriteLine($"Число {a} не является палиндромом");
+    }
+}
+
+
+
+
+
+
+
+
+
