e1288286c247c225316ae5aac6ff98f7cb83a9e5
Garly <tolya-lisichkin@yandex.ru>
2022-12-11 15:51:42 +0400

    Задача 10

---
+++ Задача 10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine());
+int result = number % 100;
+result = result / 10;
+Console.WriteLine($"Вторая цифра в числе {result}");
