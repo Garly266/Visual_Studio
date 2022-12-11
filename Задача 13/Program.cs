b01662dd66bffd0996ce6773a663f043f95c5789
Garly <tolya-lisichkin@yandex.ru>
2022-12-11 16:08:00 +0400

    Задача 13

---
+++ Задача 13/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число");
+int i = int.Parse(Console.ReadLine());
+
+if (Math.Abs(i) <= 99)
+{
+    Console.WriteLine("Нет третьего символа");
+}
+else
+{
+    string s1 = i.ToString();
+    if (i > 0 )
+    {
+        Console.WriteLine(s1[2]);
+    }
+    else
+    {
+        Console.WriteLine(s1[3]);
+    }
+}
